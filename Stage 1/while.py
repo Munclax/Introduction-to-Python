3ab4e2943eb41591646925d069ab1a38463beab0
Munchlax <premanshusampa@gmail.com>
2026-09-09 22:15:30 +0530

    Updated while

--- Stage 1/while.py
+++ Stage 1/while.py
@@ -1,4 +1,11 @@
-i=0
-while i<5:
-    print(i)
-    i+=1
+print("Welcome to the guessing game!")
+guest_input=int(input("Enter your guess"))
+num=8
+while (guest_input!=8):
+  if(guest_input<8):
+    print("Too low")
+  if(guest_input>8):
+    print("Too high")
+  guest_input=int(input("Enter another guess"))
+if(guest_input==num):
+  print("You guessed correct!")
