0af743bbda7a673b3a9d9c84c3d916885a237d46
Munchlax <premanshusampa@gmail.com>
2026-09-16 20:11:03 +0530

    More practice

---
+++ Practice session/P24.py
@@ -0,0 +1,8 @@
+#write a python program to create a list of names and print the first, third and last element of the list
+name=["A","B","C","D","E"]
+length=len(name)
+print(name)
+print(name[0])
+print(name[2])
+print(name[length-1])
+#print(name[-1])  this will also print the last element of the list
