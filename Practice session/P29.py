d2d6206e781ff3f01b4205ecd41386f560d895eb
Munchlax <premanshusampa@gmail.com>
2026-09-18 21:26:48 +0530

    Practice on sets and tuples

---
+++ Practice session/P29.py
@@ -0,0 +1,6 @@
+#write a program practicing tuples with slicing and looping
+name=("Prem","Mistu","Baba","Mum")
+print(name[1:3])
+print(name[::-1])
+for i in name:
+  print(i)
---
+++ Practice session/P30.py
@@ -0,0 +1,5 @@
+#write a program on sets and loop through the set to print only even numbers.
+sets={1,2,2,3,4,5,6,7,8}
+for i in sets:
+  if i%2==0:
+    print(i,",",end=" ")
