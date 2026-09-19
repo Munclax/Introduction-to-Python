db216cbd8cf272ebb55fb8dc3b62018810b55617
Munchlax <premanshusampa@gmail.com>
2026-09-19 22:13:15 +0530

    Added tuples

---
+++ Stage 2/tuples.py
@@ -0,0 +1,5 @@
+#tuples are similar to lists but they are immutable, meaning that once a tuple is created, its elements cannot be changed, added, or removed. Tuples are defined using parentheses () instead of square brackets [].
+numbers=(10,20,30,40,50,60,70,80,90)
+print(numbers)
+#numbers[1]=10 This wouldnt work cause of immutability i.e. you cannot change the value of an element in a tuple
+#slicing, indexing, and negative indexing work the same way as they do with lists. The only difference is that you cannot change the values of the elements in a tuple.
