e9ef754d04f3c817e1c1a71666077e3f6e467e83
Munchlax <premanshusampa@gmail.com>
2026-09-22 14:30:44 +0530

    Added Nested Dictionary

---
+++ Stage 2/nested_Dictionary.py
@@ -0,0 +1,5 @@
+#nested dictionary are dictionaries within dictionaries.
+inventory={"Apple":{"quantity":20,"price":30},"Banana":{"quantity":40,"price":15}}
+print(inventory) #print the whole dictionary
+print(inventory["Apple"]) #print the dictionary of Apple
+print(inventory["Apple"]["quantity"]) #print the quantity of Apple
