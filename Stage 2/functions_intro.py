6782b91f3dcb726d12631ea95dd4f46cc5e5d24f
Munchlax <premanshusampa@gmail.com>
2026-09-23 20:43:14 +0530

    Added introduction to functions

---
+++ Stage 2/functions_intro.py
@@ -0,0 +1,7 @@
+#functions
+def greet():
+  print("Hello. Lets start our functions journey.")
+greet()
+greet()
+greet()
+greet()
---
+++ Stage 2/functions_return.py
@@ -0,0 +1,5 @@
+def sum(a,b):
+    return a+b
+x=sum(10,20)
+print(x)
+print(sum(20,30))
